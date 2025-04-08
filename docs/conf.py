a12e28aaf0c84e757d5ea781574747ee3fd29aeb
Jamie-hb <jamiehb1498@gmail.com>
2025-04-08 17:02:18 +0100

    Update docs creation files.

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'example_package_jammy_dodger'
-copyright = '2025, James Boyle'
-author = 'James Boyle'
-release = 'Apr 2025'
+project = u'example_package_jammy_dodger'
+copyright = u'2025, James Boyle'
+author = u'James Boyle'
+release = u'Apr 2025'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['myst_nb', 'autoapi.extension', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+autoapi_dirs = ['../src']
 
-templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-autoapi_dirs = ['../src']
+
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
